d119e292362641756943c2b34ac80bba1b370fe3
tinybearc <tinybearc@gmail.com>
2016-01-20 23:09:18 +0800

    import ga for cpp

--- cpp/Classes/AppDelegate.cpp
+++ cpp/Classes/AppDelegate.cpp
@@ -1,5 +1,8 @@
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
+#ifdef SDKBOX_ENABLED
+#include "PluginGoogleAnalytics/PluginGoogleAnalytics.h"
+#endif
 
 USING_NS_CC;
 
@@ -23,6 +26,9 @@ void AppDelegate::initGLContextAttrs()
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
+#ifdef SDKBOX_ENABLED
+    sdkbox::PluginGoogleAnalytics::init();
+#endif
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
